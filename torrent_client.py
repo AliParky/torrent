4ce2da78bf7f4490aeec22e032a6be302e1c210a
AliParky <aparkinson670@gmail.com>
2025-08-13 16:27:46 +0800

    Add info hash detection in get_info_hash method

--- torrent_client.py
+++ torrent_client.py
@@ -2,4 +2,5 @@ class TorrentClient:
     def get_info_hash(self, torrent_file):
         with open(torrent_file, 'rb') as f:
             data = f.read()
+        info_start = data.find(b'4:infod')
         return
