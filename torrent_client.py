51d00ddea1c55087177ce91c8061c289bd9eddc7
AliParky <aparkinson670@gmail.com>
2025-10-12 03:27:41 +0800

    Add HTTP request to get peers from tracker in _get_peers method

--- torrent_client.py
+++ torrent_client.py
@@ -52,4 +52,5 @@ class TorrentClient:
     
     def _get_peers(self, announce_url, info_hash, length):
         params = {'info_hash': info_hash, 'peer_id': b'-PC0001-123456789012', 'port': 6881, 'uploaded': 0, 'downloaded': 0, 'left': length, 'compact': 1}
+        response = requests.get(announce_url.decode(), params=params)
         return
