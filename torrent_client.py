9011e9c812e7785eea716f87f12c3ab1209643d1
AliParky <aparkinson670@gmail.com>
2025-12-20 16:01:41 +0800

    Add detail to handshake verification comment

--- torrent_client.py
+++ torrent_client.py
@@ -83,7 +83,7 @@ class TorrentClient:
         # BitTorrent handshake
         s.send(b'\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00' + info_hash + b'-PC0001-123456789012')
         handshake = s.recv(68)
-        # Verify handshake
+        # Verify handshake - check if peer returned the same info_hash we sent
         if handshake[28:48] == info_hash:
             # Request piece 0
             s.send(struct.pack('!IB', 5, 1))  # interested
